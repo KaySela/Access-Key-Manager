ab48281724a7926e1fc14110c05effdd812a4c7f
KaySela <Kvngkhopper@gmail.com>
2024-06-03 17:20:47 +0000

    add homepage

--- KeyManager/settings.py
+++ KeyManager/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['access-key-manager-c55r.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from rest_framework_nested import routers
+from django.views.generic import TemplateView
 from . import views
 
 router = routers.DefaultRouter()
@@ -15,5 +16,6 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/', include(school_router.urls)),
     path('api/v1/activekey/', views.SchoolActiveKeyView.as_view(), name='school-active-key'),
+    path('', TemplateView.as_view(template_name='core/index.html')),
     
 ]
